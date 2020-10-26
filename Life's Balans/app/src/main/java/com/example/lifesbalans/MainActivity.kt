8ed63a527fc80bcfbae0c62ad4667727de55df9f
Daulet-Turgaliev <67974964+Daulet-Turgaliev@users.noreply.github.com>
2020-10-26 14:19:05 +0600

    Update MainActivity.kt

--- Life's Balans/app/src/main/java/com/example/lifesbalans/MainActivity.kt
+++ Life's Balans/app/src/main/java/com/example/lifesbalans/MainActivity.kt
@@ -1,52 +1,47 @@
 package com.example.lifesbalans
 
-import android.content.SharedPreferences
+import android.content.Context
 import android.os.Bundle
-import android.os.Debug
-import android.util.Log
-import android.util.Log.i
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    var balance:Int = 100
-    var count:Int = 0
-    var new_cont:Int = 0
-
-
-    var sharedPref: SharedPreferences? = null
-
-
+    var balance: Int = 100
+    var count: Int = 0
+    var new_cont: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val userId: String? = sharedPref?.getString("myPref", "default if empty")
-        if(userId != null){
 
-            textView.text = userId.toString()
+        val pref = getPreferences(Context.MODE_PRIVATE)
+        var countGet = pref.getInt("COUNT", 0)
 
-        }
+        balance = countGet
+
+        textView.setText(balance.toString())
+
         numberPicker.minValue = 0
         numberPicker.maxValue = 100
         numberPicker.value = 50
+        new_cont = 50
         numberPicker.wrapSelectorWheel = false
+
 
         numberPicker.setOnValueChangedListener { picker, oldVal, newVal ->
             new_cont = newVal
         }
 
-        button.setOnClickListener(){
-            if(checkBox.isChecked()){
+        button.setOnClickListener() {
+            if (checkBox.isChecked()) {
 
                 balance = count - new_cont
                 textView.text = "$balance"
                 count = balance
 
-            }
-            else
-            {
+            } else {
 
                 balance = count + new_cont
                 textView.text = "$balance"
@@ -55,31 +50,43 @@ class MainActivity : AppCompatActivity() {
             }
 
             textView.text = "$balance"
+
+            onSave()
         }
 
-        plusten.setOnClickListener(){
+        plusten.setOnClickListener() {
             balance = count + 10
             textView.text = "$balance"
             count = balance
-
-            val sharedPref = getSharedPreferences("myPref", MODE_PRIVATE);
-            sharedPref.edit().putString("myPref", balance.toString()).apply()
-
+            onSave()
         }
 
-        plustwenty.setOnClickListener(){
+        plustwenty.setOnClickListener() {
             balance = count + 20
             textView.text = "$balance"
             count = balance
+            onSave()
         }
 
-        minushundred.setOnClickListener(){
+        minushundred.setOnClickListener() {
             balance = count - 100
             textView.text = "$balance"
             count = balance
+            onSave()
         }
     }
 
+    private fun onSave(){
 
+        var pref = getPreferences(Context.MODE_PRIVATE)
+        var editor = pref.edit()
+
+        editor.putInt("COUNT", textView.toString().toInt())
+
+        editor.commit()
+
+        var toast = Toast.makeText(applicationContext, "Saved", Toast.LENGTH_LONG)
+        toast.show()
+    }
 }
 
